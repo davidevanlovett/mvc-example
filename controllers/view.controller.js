b564ad34b4c2e62685b5513d0a2ee52736272625
davidevanlovett@gmail.com <davidevanlovett@gmail.com>
2021-06-30 18:57:28 -0700

    Threads on Home page, indiviudal thread Pages

--- controllers/view.controller.js
+++ controllers/view.controller.js
@@ -2,44 +2,50 @@ const router = require('express').Router();
 const { Thread, Post, User } = require('../models');
 const SHOULD_LOG = process.env.NODE_ENV === 'develop';
 
+
 router.get('/', async (req, res) => {
-	res.render('home');
-});
-
-router.get('/threads', async (req, res) => {
 	try {
-		const threads = await Thread.findAll({include: Post, logging: SHOULD_LOG});
-		const plainThreads = threads.map(thread => thread.get({ plain: true}));
-		res.render('threads', {threads: plainThreads});
+		const threads = await Thread.findAll({ include: Post, logging: SHOULD_LOG });
+		const plainThreads = threads.map(thread => thread.get({ plain: true }));
+		res.render('home', { threads: plainThreads });
 	}
-	catch (err){
+	catch (err) {
 		console.error(err);
-		res.render('error', {error: SHOULD_LOG ? err : 'Internal Server Error'})
+		res.render('error', { error: SHOULD_LOG ? err : 'Internal Server Error' })
 	}
 });
 
-router.get('/posts', async (req, res) => {
+router.get('/threads/:id', async (req, res) => {
 	try {
-		const posts = await Post.findAll({include: Thread, logging: SHOULD_LOG});
-		const plainPosts = posts.map(post => post.get({ plain: true}));
-		res.render('posts', {posts: plainPosts});
+		const thread = await Thread.findOne({
+			where: {
+				id: req.params.id
+			},
+			include: [{
+				model: Post, 
+				include: {
+					model: User
+				}
+			}], logging: SHOULD_LOG
+		});
+		const plainThread = thread.get({ plain: true });
+		res.render('thread', { thread: plainThread, posts: plainThread.Posts });
 	}
-	catch (err){
+	catch (err) {
 		console.error(err);
-		res.render('error', {error: SHOULD_LOG ? err : 'Internal Server Error'})
+		res.render('error', { error: SHOULD_LOG ? err : 'Internal Server Error' })
 	}
 });
-
 
 router.get('/users', async (req, res) => {
 	try {
-		const users = await User.findAll({include: Post, logging: SHOULD_LOG});
-		const plainUsers = users.map(user => user.get({ plain: true}));
-		res.render('users', {users: plainUsers});
+		const users = await User.findAll({ include: Post, logging: SHOULD_LOG });
+		const plainUsers = users.map(user => user.get({ plain: true }));
+		res.render('users', { users: plainUsers });
 	}
-	catch (err){
+	catch (err) {
 		console.error(err);
-		res.render('error', {error: SHOULD_LOG ? err : 'Internal Server Error'})
+		res.render('error', { error: SHOULD_LOG ? err : 'Internal Server Error' })
 	}
 });
 
