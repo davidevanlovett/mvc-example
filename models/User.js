adf01d9286e95be1fbdc5bc626bc79142a9b736b
davidevanlovett@gmail.com <davidevanlovett@gmail.com>
2021-06-30 17:41:55 -0700

    User can salt password and checkpassword

--- models/User.js
+++ models/User.js
@@ -1,17 +1,34 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = require("../connection/connection");
+const bcrypt = require('bcrypt');
 
-class User extends Model {};
+class User extends Model {
+	checkPassword(pw){
+		return bcrypt.compareSync(pw, this.password)
+	}
+};
 
 User.init({
 	email: {
-		type: DataTypes.STRING
+		type: DataTypes.STRING,
+		allowNull: false
 	},
 	password: {
-		type: DataTypes.STRING
+		type: DataTypes.STRING,
+		allowNull: false
 	}
 },{
-	sequelize
+	sequelize,
+	hooks: {
+		async beforeCreate(user){
+			user.password = await bcrypt.hash(user.password, 10);
+			return user;
+		},
+		async beforeUpdate(user){
+			user.password = await bcrypt.hash(user.password, 10);
+			return user;
+		}
+	}
 });
 
 
