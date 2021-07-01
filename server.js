4896f06dc6779acc8de3142fbe2b4b1129f53b02
davidevanlovett@gmail.com <davidevanlovett@gmail.com>
2021-06-30 17:18:46 -0700

    Express-Handlebars Setup

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+	res.render('home');
+});
 
 app.listen(PORT, () => {
 	console.info(`Server is listening at ${PORT}`);
